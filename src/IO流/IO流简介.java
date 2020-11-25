d3f1d8d01277a04a016dba616e3d59c8c4e78ab4
softpeople <66582421+softpeople@users.noreply.github.com>
2020-11-25 23:53:34 +0800

    1

---
+++ src/IO流/IO流简介.java
@@ -0,0 +1,34 @@
+package IO流;
+/*
+I(Input)/O(Output)流,是Java中用来传输数据的方式
+按数据流分：
+        输入流
+        输出流
+按操作方式分：
+        字节流：
+           InputStream
+           OutputStream
+        字符流：
+           Reader
+           Writer
+IO流体系
+    字符流：按字符读写数据的IO流
+            Reader：字符输入流的顶层抽象类
+               FileReader       普通的字符输入流
+               BufferedReader   高效的字符输入流（也叫：字符缓冲输入流）
+            Writer：字符输出流的顶层抽象类
+                FileWriter      普通的字符输出流
+                BufferedWriter  高效的字符输出流（也叫：字符缓冲输出流）
+    字节流：按字节读写数据的IO流
+            InputStream
+                FileInputStream         普通的字节输入流
+                ButteredInputStream     高效的字节输入流（也叫：字节缓冲输入流）
+            OutputStream
+                FileOutputStream        普通的字节输出流
+                BufferedOutputStream    高效的字节输出流（也叫：字节缓冲输出流）
+ */
+public class IO流简介 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/IO流/抛出异常.java
+++ src/IO流/抛出异常.java
@@ -1,4 +1,20 @@
 package IO流;
-
+/*
+    异常处理方式二:throws
+        抛出异常交给调用者处理
+ */
 public class 抛出异常 {
+    public static void show(){      //不用静态就会报错：无法从静态上下文中引用非静态 方法
+        System.out.println(10/0);
+    }
+    public static void main(String[] args) throws Exception{   //使用抛出异常会终止程序
+
+        try {
+            show();
+        } catch (Exception e) {
+            System.out.println("异常");           //程序会继续执行
+        }
+        System.out.println("try..catch..会继续执行程序");
+    }
+
 }

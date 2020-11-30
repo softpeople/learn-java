cfbb01f541e5f7bdc4805cf4982327f54c065b30
softpeople <66582421+softpeople@users.noreply.github.com>
2020-12-01 00:25:29 +0800

    1

--- src/IO流/File类.java
+++ src/IO流/File类.java
@@ -15,6 +15,14 @@ import java.io.File;
     isDirectory:判断File对象是否为目录
     isFile():判断File对象是否为文件
     exists():判断File对象是否存在
+   获取
+    getAbsolutePath():获取文件的绝对路径
+    getPath():获取文件的相对路径
+    list(): 获取指定目录下所有文件（夹）名称数组
+    listFile():获取指定目录下所有文件（夹）File数组
+
+    绝对路径：以盘符开头的路径，固定的，写“死”的路径；
+    相对路径：意思是相对于某个路径而言，Java中的相对路径是指：相对于当前项目发路径来讲
  */
 public class File类 {
     public static void main(String[] args) throws Exception {
@@ -50,5 +58,24 @@ public class File类 {
         System.out.println("判断h是不是文件夹"+h.isDirectory());
         System.out.println("判断h是不是文件"+h.isFile());
         System.out.println("判断h是否存在"+h.exists());
+
+        System.out.println("---------------------------------华丽的分割线");
+
+        //获取a的绝对路径和绝对路径
+        System.out.println(a.getAbsolutePath());    //绝对路径
+        System.out.println(a.getPath());            //相对路径
+        //获取文件名
+        System.out.println(a.getName());
+        System.out.println("---------------------------------华丽的分割线");
+        //获取c目录下所有文件（夹）的名称数组String[]
+        for(String name:c.list())
+        {
+            System.out.println(name);
+        }
+        //获取目录下所有文件（夹）File数组
+        for (File name:c.listFiles())
+        {
+            System.out.println(name);
+        }
     }
 }

7dbfe761f133bde52bd5550ab25efdfd2f636b13
softpeople <66582421+softpeople@users.noreply.github.com>
2020-12-08 23:21:05 +0800

    1

---
+++ src/反射/反射方式获取成员变量并使用.java
@@ -0,0 +1,9 @@
+package 反射;
+/*
+
+ */
+public class 反射方式获取成员变量并使用 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/反射/反射方式获取成员方法并使用.java
@@ -0,0 +1,12 @@
+package 反射;
+/*
+    Class类
+        成员方法:
+            public Method getMethob()
+ */
+public class 反射方式获取成员方法并使用 {
+    public static void main(String[] args) throws Exception{
+        //1.获取Student类的字节码文件
+        Class a=Class.forName("反射.student");
+    }
+}
